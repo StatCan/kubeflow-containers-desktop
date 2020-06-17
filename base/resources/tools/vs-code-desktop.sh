94db99f761dcbc5030a77733f1ca43dc51dc1815
Frances Zsurka <franceszsurka@gmail.com>
2020-06-17 17:00:39 -0400

    feat: update vs code

--- base/resources/tools/vs-code-desktop.sh
+++ base/resources/tools/vs-code-desktop.sh
@@ -3,12 +3,13 @@
 # Stops script execution if a command has an error
 set -e
 
-SHA256=7b3bce26aff8677ae8920490709839dc6c5fda83728aec09724245761b64f217
+SHA256=6c117339d77b9593ad20b6eb4601ff7d0fd468922550d500edf07e3071e9a041
+VERSION=1.46.0
 
 if [ ! -f "/usr/share/code/code" ]; then
     echo "Installing VS Code. Please wait..."
     cd $RESOURCES_PATH
-    wget -q https://go.microsoft.com/fwlink/?LinkID=760868 -O ./vscode.deb
+    wget -q https://update.code.visualstudio.com/${VERSION}/linux-deb-x64/stable -O ./vscode.deb
     echo "${SHA256} ./vscode.deb" | sha256sum -c -
     apt-get update
     apt-get install -y ./vscode.deb
